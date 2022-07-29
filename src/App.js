62acce1d5098dce7192aee20f52e9db36ea8cfa2
Olivia Giandrea <olivia.giandrea@gmail.com>
2022-07-28 20:03:19 -0400

    build static site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./components/Expenses";
+
+// import "./App.css";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "ramen",
+      amount: 3.73,
+      date: new Date(2019, 6, 7),
+    },
+    {
+      id: "e2",
+      title: "yogurt",
+      amount: 6.26,
+      date: new Date(2002, 0, 11),
+    },
+    {
+      id: "e3",
+      title: "hat",
+      amount: 14.92,
+      date: new Date(2018, 5, 26),
+    },
+    {
+      id: "e4",
+      title: "carupisu",
+      amount: 1.01,
+      date: new Date(2018, 6, 31),
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Expense Tracker</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
